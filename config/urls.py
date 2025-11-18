c1862ac5ef2af65b2ad48a768796f9fe6073a7a6
Mmy2000 <mm.yousef811@gmail.com>
2025-11-19 00:27:57 +0200

    feat : make debug = False

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from site_settings.views import custom_404_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('content.urls')),
     # path('summernote/', include('django_summernote.urls')),
 ]
+
+urlpatterns.append(re_path(r'^.*$', custom_404_view))  # Placeholder for custom 404 view
--- site_settings/views.py
+++ site_settings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def custom_404_view(request, exception=None):
+    return render(request, "404.html", {}, status=404)
