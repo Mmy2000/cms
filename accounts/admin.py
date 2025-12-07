7483104768aea296240aaa79262d5faa7af13f71
Mmy2000 <mm.yousef811@gmail.com>
2025-12-08 01:06:15 +0200

    feat: finish login and register logic

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin as UnfoldModelAdmin
 from .models import Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
+
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+
 # Register your models here.
 
 @admin.register(Profile)
@@ -18,4 +24,22 @@ class ProfileAdmin(UnfoldModelAdmin):
             'classes': ('collapse',),
         }),
     )
-    
+
+
+# User Admin
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, UnfoldModelAdmin):
+    # Forms loaded from `unfold.forms`
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, UnfoldModelAdmin):
+    pass
